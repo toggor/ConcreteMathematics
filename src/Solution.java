0113ab88a76a15c6186e8468c6027162511e8d5e
toggor <toggor@gmail.com>
2017-01-18 12:33:16 +0300

    Hanoi Tower working

--- src/Solution.java
+++ src/Solution.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
+import java.io.*;
+
 
 public class Solution {
     public static void main(String[] args)
     {
         ArrayList<String> s_from= new ArrayList<String>(), s_mid= new ArrayList<String>(), s_to = new ArrayList<String>();
-        s_from.add("stick_1");
+        Integer t_size = 0;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please enter number for the size of tower.");
+
+
+        while (!(t_size>=1 && t_size<=8)) {
+            try {
+                System.out.println("It should be between 1 and 8 not to wait too long");
+                t_size = Integer.valueOf(reader.readLine());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println(e.getClass());
+            }
+        }
+
+        s_from = setTower(t_size);
         s_mid.add("stick_2");
         s_to.add("stick_3");
-
-
-        s_from.add("4");
-        s_from.add("3");
-        s_from.add("2");
-        s_from.add("1");
-
-        for(int i=s_from.size()-1; i>0; i--){
-            System.out.println(s_from.get(i));
-        }
 
 
         System.out.println(s_from.size()-1);
@@ -26,6 +34,16 @@ public class Solution {
 
     }
 
+    public static ArrayList<String> setTower (int n){
+        ArrayList<String> s_from= new ArrayList<String>();
+        s_from.add("stick_1");
+            for(int i=n; i>0; i--){
+                //System.out.println(s_from.get(i));
+                s_from.add(String.valueOf(i));
+            }
+            return s_from;
+
+    }
 
     public static void moveRings (ArrayList<String> s_from, ArrayList<String> s_mid, ArrayList<String> s_to, Integer number){
         if (number <= 0){
